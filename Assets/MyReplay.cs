548e854b52553a3b0c268e502b55cd85d7fb5698
Nicolla Elkhouri <nicollae@entangleinteractive.com>
2017-03-29 23:42:30 +1100

    add myreplay script

---
+++ Assets/MyReplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyReplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+public class MyKeyFrame
+{
+    public float myTime;
+    public  Vector3 pos;
+    public  Quaternion rot;
+
+    public MyKeyFrame(float atime, Vector3 apos,Quaternion arot)
+    {
+        myTime = atime;
+        pos = apos;
+        rot = arot;
+    }
+}
